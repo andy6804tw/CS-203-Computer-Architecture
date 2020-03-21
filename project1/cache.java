8cdcf73b47e1241d1813d0c084f106f06caadcf7
Tsai Yi Lin <andy6804tw@yahoo.com.tw>
2020-03-21 16:06:33 +0800

    done direct map

--- project1/cache.java
+++ project1/cache.java
@@ -10,20 +10,52 @@ class Address {
   }
 }
 
+class Block {
+  int tag = 0;
+  int valid = 0;
+  public Block(int tag, int valid) {
+    this.tag = tag;
+    this.valid = valid;
+  }
+}
+
 public class cache {
-  public int cache_size=128; // Cache的大小，單位為KByte
-  public int block_size = 8; // 每個Cache Block的大小，單位為Byte
-  public int set_degree = 1; // 一個set中的cache block個數
-  public int hitCount=0;
-  public int missCount=0;
+  public static int cache_size = 1024; // Cache的大小，單位為KByte
+  public static int block_size = 16; // 每個Cache Block的大小，單位為Byte
+  public static int set_degree = 1; // 一個set中的cache block個數
+  public static int setSize = 0;
+  public static int hitCount = 0;
+  public static int missCount = 0;
+  public static Block[] setArray;
 
   public static ArrayList<Address> addressList = new ArrayList<>();
+
   public static void main(String[] args){
 
-    readFile("example.txt");
-    for(int i=0;i<addressList.size();i++){
-      System.out.println(addressList.get(i).HEX_address+" "+addressList.get(i).DEC_address);
+    // 計算set的個數
+    setSize = cache_size/(block_size*set_degree);
+    // 初始化 setArray
+    setArray=new Block[setSize];
+    for(int i=0;i<setSize;i++){
+      setArray[i]=new Block(0,0);
     }
+    readFile("example3.txt");
+    for(int i=0;i<addressList.size();i++){
+      int position=addressList.get(i).DEC_address/block_size; // 取得記憶體位置
+      int setIndex=position%setSize;
+      int tag=position/setSize;
+      // n-way nee loop
+      if(setArray[setIndex].valid!=0 && setArray[setIndex].tag==tag){
+        hitCount++;
+      }else{
+        setArray[setIndex].valid=1;
+        setArray[setIndex].tag=tag;
+        missCount++;
+      }
+      // System.out.println(addressList.get(i).HEX_address+" "+addressList.get(i).DEC_address);
+    }
+    System.out.println("Hit Count: "+ hitCount);
+    System.out.println("Miss Count: " + missCount);
     
   }
   /**
